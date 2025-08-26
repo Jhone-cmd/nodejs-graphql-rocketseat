e9bcf3b2b46c47e64670c18ec5903cff0b80eb5d
Jhone-cmd <jhone.1941@gmail.com>
2025-08-25 21:11:36 -0300

    chore: configured the typeform to connect to the database

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import express from 'express'
+import { AppDataSource } from './config/db/data-source'
 
 export const app = express()
+
+async function init() {
+  await AppDataSource.initialize()
+}
+
+init()
---
+++ src/config/db/data-source.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'docker',
+  password: 'docker',
+  database: 'graphql',
+  synchronize: true,
+  logging: true,
+  entities: [],
+})
