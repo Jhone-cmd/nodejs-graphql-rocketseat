8aec6f5c64c163ece7619984a6cb81f2c27d39dc
Jhone-cmd <jhone.1941@gmail.com>
2025-08-27 09:47:51 -0300

    chore: created object type of the User entity, and made a query to test

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,5 @@
 import { hash } from 'bcryptjs'
+import { Field, ID, ObjectType } from 'type-graphql'
 import {
   BaseEntity,
   BeforeInsert,
@@ -8,22 +9,28 @@ import {
 } from 'typeorm'
 
 @Entity('users')
+@ObjectType()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
   id: string
 
   @Column()
+  @Field()
   first_name: string
 
   @Column()
+  @Field()
   last_name: string
 
   @Column({ unique: true })
+  @Field()
   email: string
 
   password: string
 
   @Column()
+  @Field()
   password_hash: string
 
   @BeforeInsert()
--- src/graphql/resolvers/user-resolver.ts
+++ src/graphql/resolvers/user-resolver.ts
@@ -1,12 +1,12 @@
 import { Query, Resolver } from 'type-graphql'
+import { User } from '../../entities/User'
 
 @Resolver()
 export class UserResolver {
-  private users = ['test1', 'test2', 'test3']
+  // private users = ['test1', 'test2', 'test3']
 
-  @Query(() => [String])
-  async getUsers(): Promise<string[]> {
-    return this.users
+  @Query(() => [User])
+  async getUsers(): Promise<User[]> {
+    return await User.find()
   }
-  
 }
