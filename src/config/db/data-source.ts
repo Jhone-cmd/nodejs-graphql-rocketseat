ccadbdf0312af868fbf167d2127ffbd19fba794e
Jhone-cmd <jhone.1941@gmail.com>
2025-08-29 09:37:22 -0300

    chore: created the environment variables and add the .env.example

--- src/config/db/data-source.ts
+++ src/config/db/data-source.ts
@@ -1,12 +1,13 @@
 import { DataSource } from 'typeorm'
+import { env } from '../../env/schema'
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'docker',
-  password: 'docker',
-  database: 'graphql',
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_DATABASE,
   synchronize: true,
   logging: true,
   entities: ['src/entities/**/*.ts'],
--- src/env/schema.ts
+++ src/env/schema.ts
@@ -3,6 +3,11 @@ import 'dotenv/config'
 
 const schema = z.object({
   PORT: z.coerce.number().default(3333),
+  DB_HOST: z.string(),
+  DB_PORT: z.coerce.number().default(5432),
+  DB_USERNAME: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_DATABASE: z.string(),
 })
 
 export const env = schema.parse(process.env)
